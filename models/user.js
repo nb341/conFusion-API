d919c7be58450bf8db4a33dca1967273dd6ddb6a
Narindra Balkissoon <narindrab4@gmail.com>
2021-04-15 17:32:50 -0400

    Passport

--- models/user.js
+++ models/user.js
@@ -1,22 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
     admin:   {
         type: Boolean,
         default: false
     }
 });
 
-var User = mongoose.model('User', User);
+User.plugin(passportLocalMongoose);
+ 
 
-module.exports = User;
+module.exports = mongoose.model('User', User);
